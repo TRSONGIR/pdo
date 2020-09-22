a22111206f551834c3042d1b88d996c01e68a1f0
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-09-23 00:42:12 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-$host = "ec2-34-253-148-186.eu-west-1.compute.amazonaws.com";
-$dbname="d5pj2kv8rvk48l";
-$dbuser="woziqaocstlvip";
-$dbpass="3f440fcba04d85a681467952a3675d13ae4af23d8286b6c799f7f9b76e535825";
 try{
-    $pdo = new PDO('pgsql:host=$host;dbname=$dbname',$dbuser,$dbpass);
+    $pdo = new PDO('pgsql:host=ec2-34-253-148-186.eu-west-1.compute.amazonaws.com;dbname=d5pj2kv8rvk48l','woziqaocstlvip','3f440fcba04d85a681467952a3675d13ae4af23d8286b6c799f7f9b76e535825');
 }catch (PDOException $e){
     die($e->getMessage());
 }
